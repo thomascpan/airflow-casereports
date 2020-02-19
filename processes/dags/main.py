d160043c7d845390443ca80dde2c2037730af1e5
carolynyen <carolynyen@Carolyns-MacBook-Pro-2.local>
2020-02-18 16:19:56 -0800

    set up airflow

---
+++ processes/dags/main.py
@@ -0,0 +1,73 @@
+from datetime import timedelta
+
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(2),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'adhoc':False,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'trigger_rule': u'all_success'
+}
+
+dag = DAG(
+    'main',
+    default_args=default_args,
+    description='Casereport processing DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+t1 = BashOperator(
+    task_id='load_data',
+    bash_command='date',
+    dag=dag,
+)
+
+t1.doc_md = """\
+#### Task Documentation
+"""
+
+dag.doc_md = __doc__
+
+t2 = BashOperator(
+    task_id='process',
+    depends_on_past=False,
+    bash_command='sleep 5',
+    dag=dag,
+)
+
+templated_command = """
+{% for i in range(5) %}
+    echo "{{ ds }}"
+    echo "{{ macros.ds_add(ds, 7)}}"
+    echo "{{ params.my_param }}"
+{% endfor %}
+"""
+
+t3 = BashOperator(
+    task_id='mongoDB',
+    depends_on_past=False,
+    bash_command=templated_command,
+    params={'my_param': 'Parameter I passed in'},
+    dag=dag,
+)
+
+t1 >> t2 >> t3
