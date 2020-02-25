27492eb2f1b26c26579fe5ed628db5e2aa886962
carolynyen <carolynyen@Carolyns-MacBook-Pro-2.local>
2020-02-24 21:50:27 -0800

    added ftplib

--- processes/dags/upload_to_s3.py
+++ processes/dags/upload_to_s3.py
@@ -6,6 +6,10 @@ from airflow.operators.python_operator import PythonOperator
 import airflow.hooks.S3_hook
 import datetime
 import os
+import logging
+import urllib
+from ftplib import FTP
+
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 my_file = os.path.join(THIS_FOLDER, 'test.txt')
@@ -13,6 +17,43 @@ my_file = os.path.join(THIS_FOLDER, 'test.txt')
 def upload_file_to_S3_with_hook(filename, key, bucket_name):
     hook = airflow.hooks.S3_hook.S3Hook('my_conn_S3')
     hook.load_file(filename, key, bucket_name)
+
+def import_wrapper():
+    logging.info("start import wrapper")
+    download_files()
+    #decompress_files()
+    #delete_extra_files()
+    #compress_files()
+    return None
+    #upload_file_to_S3_with_hook(my_file, 'newfile.txt', 'supreme-acrobat')
+
+def download_files():
+    #path = 'pub/pmc/oa_bulk'
+    #files = []
+    # filenames = ftp.nlst('pub/pmc/oa_bulk/*.xml.tar.gz')
+    ftp = FTP("ftp.ncbi.nlm.nih.gov")
+    ftp.login("anonymous", "ifso6888@gmail.com")
+    #ftp.cwd(path)
+    #filenames = ftp.nlst('pub/pmc/oa_bulk/comm_use.A-B.xml.tar.gz')
+    #ftp.dir(files.append)
+    #logging.info(files)
+
+    #for filename in filenames:
+        #ftp.open(filename, 'wb').write)
+        #ftp.retrbinary("RETR " + filename)
+    ftp.quit()
+    #upload_file_to_S3_with_hook(my_file, 'newfile2.txt', 'supreme-acrobat')
+    logging.info("done download")
+    return None
+
+def decompress_files():
+    pass
+
+def delete_extra_files():
+    pass
+
+def compress_files():
+    pass
 
 default_args = {
     'owner': 'airflow',
@@ -24,7 +65,6 @@ default_args = {
     #"ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/non_comm_use.A-B.xml.tar.gz"
 #    for file in files:
         # download file
-        # uncompress
         # untar
         # delete non-case-reports
         # tar
@@ -37,19 +77,9 @@ with DAG('S3_dag_test', default_args=default_args, schedule_interval='@once') as
             task_id='dummy_start'
     )
 
-    upload_to_S3_task = PythonOperator(
-            task_id='upload_file_to_S3',
-            python_callable=lambda: print("Uploading file to S3")
+    import_files_task = PythonOperator(
+        task_id = 'import_wrapper',
+        python_callable=import_wrapper
     )
 
-    start_task >> upload_to_S3_task
-
-upload_to_S3_task = PythonOperator(
-    task_id='upload_file_to_S3_with_hook',
-    python_callable=upload_file_to_S3_with_hook,
-    op_kwargs={
-        'filename': my_file,
-        'key': 'new3.txt',
-        'bucket_name': 'supreme-acrobat',
-    },
-    dag=dag)
+    start_task >> import_files_task
