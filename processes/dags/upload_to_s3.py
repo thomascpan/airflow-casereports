902b9dd13d83b52a83f4b99b4915e460c199faa3
carolynyen <carolynyen@Carolyns-MacBook-Pro-2.local>
2020-02-24 19:15:28 -0800

    cleanup

--- processes/dags/upload_to_s3.py
+++ processes/dags/upload_to_s3.py
@@ -3,23 +3,12 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-import boto3
 import airflow.hooks.S3_hook
 import datetime
 import os
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 my_file = os.path.join(THIS_FOLDER, 'test.txt')
-from boto3.s3.transfer import S3Transfer
-
-#client = boto3.client('s3', aws_access_key_id='',aws_secret_access_key='')
-#transfer = S3Transfer(client)
-#transfer.upload_file(my_file, 'supreme-acrobat', 'blah.txt')
-
-def upload_file_to_S3(filename, key, bucket_name):
-    transfer = S3Transfer(client)
-    transfer.upload_file(filename, bucket_name, key)
-    s3.Bucket(bucket_name).upload_file(filename, key)
 
 def upload_file_to_S3_with_hook(filename, key, bucket_name):
     hook = airflow.hooks.S3_hook.S3Hook('my_conn_S3')
@@ -30,6 +19,17 @@ default_args = {
     'start_date': datetime.datetime(2020, 2, 1),
     'retry_delay': datetime.timedelta(minutes=5)
 }
+
+#files = files("ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/*.xml.tar.gz")
+    #"ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/non_comm_use.A-B.xml.tar.gz"
+#    for file in files:
+        # download file
+        # uncompress
+        # untar
+        # delete non-case-reports
+        # tar
+        # compress
+#        upload_file_to_S3_with_hook(filename, key, bucket_name)
 
 with DAG('S3_dag_test', default_args=default_args, schedule_interval='@once') as dag:
 
@@ -49,7 +49,7 @@ upload_to_S3_task = PythonOperator(
     python_callable=upload_file_to_S3_with_hook,
     op_kwargs={
         'filename': my_file,
-        'key': 'new2.txt',
+        'key': 'new3.txt',
         'bucket_name': 'supreme-acrobat',
     },
     dag=dag)
